e2e7a9ffd6fe700c3266ac3736e72f0c798737ac
Etienne Baqué <etiennebaque@gmail.com>
2017-05-28 21:28:44 -0400

    Created signed-in and login resources

---
+++ app/components/login-page.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+const { service } = Ember.inject;
+
+export default Ember.Component.extend({
+  session: service(),
+  actions: {
+    authenticate() {
+    },
+  },
+});
--- app/router.js
+++ app/router.js
@@ -7,8 +7,11 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('books', { path: '/' });
-  this.route('author', { path: '/author/:author_id' });
+  this.route('login');
+  this.route('signed-in', function(){
+    this.route('books', { path: '/' });
+    this.route('author', { path: '/author/:author_id' });
+  });
 });
 
 export default Router;
